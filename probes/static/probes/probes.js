aae6d7124e8573d4f0e92c79d0c62cd0711814cd
Luke Gilbert <ltgilbert@sep.com>
2016-06-09 15:02:37 -0400

    Persist expanded/collapsed probe tab state as a Cookie.

--- probes/static/probes/probes.js
+++ probes/static/probes/probes.js
@@ -1,3 +1,32 @@
+var probes_tabs_expanded_cookie_name = 'probes_tabs_expanded';
+
+function create_cookie(name, value, days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days * 24 * 60 * 60 * 1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function read_cookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) {
+		    return c.substring(nameEQ.length, c.length);
+		}
+	}
+	return null;
+}
+
+function erase_cookie(name) {
+	create_cookie(name, "", -1);
+}
+
 function are_tabs_scrollable() {
     return $('.probe-tab-container').hasClass('scrollable-tabs');
 }
@@ -46,5 +75,6 @@ function toggle_scrollable_probe_tabs() {
         $('#probe-tab-style-toggle').html('&#8862; Expand');
         enable_disable_tab_scroll_buttons();
     }
+    create_cookie(probes_tabs_expanded_cookie_name, are_tabs_scrollable() ? 0 : 1);
     return false;
 }
